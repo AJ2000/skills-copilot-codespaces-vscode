8f3357458f0bed5ee793399e26e0296ab10ad8aa
AJ2000 <andihall@gmail.com>
2024-08-29 06:48:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+
+function skillsMember () {
+    var skills = {
+        "name": "Rohit",
+        "skills": ["C++", "Java", "Python", "JavaScript", "NodeJS", "ReactJS", "AngularJS", "Django", "Flask", "Spring", "Hibernate", "JPA", "SQL", "MongoDB", "Cassandra"],
+        "experience": 5
+    };
+
+    return skills;
+}
